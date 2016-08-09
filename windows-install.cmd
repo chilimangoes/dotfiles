965380cae99cf94929f96e8237b3d46c6ee335c6
chilimangoes <chilimangoes@users.noreply.github.com>
2016-08-09 13:27:19 -0700

    Adding ConEmu settings

--- windows-install.cmd
+++ windows-install.cmd
@@ -1,10 +1,14 @@
+@echo off
+
+set GITHUB_REPO=https://github.com/chilimangoes/dotfiles.git
+set CONEMU_DIR=C:\Program Files\ConEmu
 
 @if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
 @set APP_DIR=%HOME%\dotfiles
 IF NOT EXIST "%APP_DIR%" (
-    call git clone --recursive https://github.com/chilimangoes/dotfiles.git "%APP_DIR%"
+    call git clone --recursive %GITHUB_REPO% "%APP_DIR%"
 ) ELSE (
 	@set ORIGINAL_DIR=%CD%
     echo updating dotfiles
@@ -14,6 +18,14 @@ IF NOT EXIST "%APP_DIR%" (
 	call cd "%APP_DIR%" 
 )
 
+if exist "%HOME%\.vimrc" del "%HOME%\.vimrc"
 call mklink "%HOME%\.vimrc" "%APP_DIR%\vim\.vimrc"
+
+if exist "%HOME%\.vsvimrc" del "%HOME%\.vsvimrc"
 call mklink "%HOME%\.vsvimrc" "%APP_DIR%\vim\.vsvimrc"
 
+if exist "%CONEMU_DIR%\ConEmu.xml" del "%CONEMU_DIR%\ConEmu.xml"
+call mklink "%CONEMU_DIR%\ConEmu.xml" "%APP_DIR%\ConEmu\ConEmu.xml"
+
+if exist "%CONEMU_DIR%\conemu_startup.txt" del "%CONEMU_DIR%\conemu_startup.txt"
+call mklink "%CONEMU_DIR%\conemu_startup.txt" "%APP_DIR%\ConEmu\conemu_startup.txt"
