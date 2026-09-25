c5cdb3ae12e6cdd9ad61e467f23f3a5e375f4d1b
Jacob Eggleston <chilimangoes@users.noreply.github.com>
2026-09-25 15:59:56 -0500

    remove conemu from install, add git aliases

--- windows-install.cmd
+++ windows-install.cmd
@@ -1,7 +1,6 @@
 @echo off
 
 set GITHUB_REPO=https://github.com/chilimangoes/dotfiles.git
-set CONEMU_DIR=C:\Program Files\ConEmu
 
 @if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
@@ -23,11 +22,5 @@ call mklink "%HOME%\.vimrc" "%APP_DIR%\vim\.vimrc"
 
 if exist "%HOME%\.vsvimrc" del "%HOME%\.vsvimrc"
 call mklink "%HOME%\.vsvimrc" "%APP_DIR%\vim\.vsvimrc"
-
-if exist "%CONEMU_DIR%\ConEmu.xml" del "%CONEMU_DIR%\ConEmu.xml"
-call mklink "%CONEMU_DIR%\ConEmu.xml" "%APP_DIR%\ConEmu\ConEmu.xml"
-
-if exist "%CONEMU_DIR%\conemu_startup.txt" del "%CONEMU_DIR%\conemu_startup.txt"
-call mklink "%CONEMU_DIR%\conemu_startup.txt" "%APP_DIR%\ConEmu\conemu_startup.txt"
 
 pause
